e66ab73bcaeedc60a98d1e2eeee25db509f23f77
Jose Mauro <z.mauro@live.com>
2019-08-24 00:21:23 -0300

    step 2 folder with script to plot charts

--- 1_build_networks/build_networks.py
+++ 1_build_networks/build_networks.py
@@ -1,6 +1,7 @@
-# in (Onella, 2007) paper. The first network have an edge between
+# in (Onella, 2007) paper there are 2 types of network.
+# The first network (non mutual) have an edge between
 # two nodes if there is at least one call between them
-# (A called B OR B called A). The second network have and edge
+# (A called B OR B called A). The second network (mutual)  have and edge
 # between two nodes if and only if both of them called to each
 # other (A called B AND B called A).
 
@@ -11,15 +12,14 @@ import os
 files_list = []
 
 for file_name in os.listdir("./data"):
-    if file_name.endswith("*.out"):
+    if file_name.endswith(".out"):
         files_list.append(file_name)
-
-g1 = Graph()
 
 ncol_out_file = open("graph.ncol","w")
 
 for file_name in files_list:
-    with open(file_name, 'r') as csvfile:
+    dir_name = "./data/" + file_name
+    with open(dir_name, 'r') as csvfile:
         csv_read = csv.reader(csvfile, delimiter=';')
         #add nodes
         name_list = []
@@ -27,3 +27,17 @@ for file_name in files_list:
             line = row[4] + " " + row[6] + " " + row[2] + "\n" 
             ncol_out_file.write(line)
 
+ncol_out_file.close()
+
+# To generate the two types of network
+
+g_nonmutual = Graph()
+g_mutual = Graph()
+
+
+#g_nonmutual.Read_Ncol("graph.ncol", weights=True, directed=True)
+#g_nonmutual.to_undirected(mode="collapse")
+'''
+g_mutual.Read_Ncol("graph.ncol", weights=True, directed=True)
+g_mutual.to_undirected(mode="mutual")
+'''

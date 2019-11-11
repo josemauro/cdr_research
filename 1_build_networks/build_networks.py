d0aae6e680b1ce4981f0333541ce8b9016f3f924
Jose Mauro <z.mauro@live.com>
2019-11-11 19:42:47 -0300

    decisoes da 08-11-19

--- 1_build_networks/build_networks.py
+++ 1_build_networks/build_networks.py
@@ -1,17 +1,14 @@
-# in (Onella, 2007) paper there are 2 types of network.
-# The first network (non mutual) have an edge between
-# two nodes if there is at least one call between them
-# (A called B OR B called A). The second network (mutual)  have and edge
-# between two nodes if and only if both of them called to each
-# other (A called B AND B called A).
+"""This script create one social network from files in "./data" folder and save
+this network in ncol file format called "graph.ncol".
+"""
 
 import csv
 from igraph import Graph
-import os
+
 
 files_list = []
 
-for file_name in os.listdir("./data"):
+for file_name in ios.listdir("./data"):
     if file_name.endswith(".out"):
         files_list.append(file_name)
 
@@ -29,15 +26,60 @@ for file_name in files_list:
 
 ncol_out_file.close()
 
-# To generate the two types of network
+"""
+In (Onella, 2007) paper there are 2 types of network:
 
+    -Non-mutual: connected 2 users with an undirected link if there had been 
+     at least one phone call between. (A called B OR B called A)
+
+    -Mutual: connected 2 users with an undirected link if there had been at 
+    least one reciprocated pair of phone calls between them.
+    (A called B AND B called A)
+
+The social network created above (graph.ncol) in this script can be read in one
+of this two formats.
+"""
+
+
+# Code to read ncol file as non-mutual network:
+'''
 g_nonmutual = Graph()
+g_nonmutual.Read_Ncol("graph.ncol", weights=True, directed=True)
+
+# Remove arrows from each edge.
+g_nonmutual.to_undirected(mode="each")
+
+# Simplifies a graph by removing self-loops and/or multiple edges. 
+g_nonmutual = g_nonmutual.simplify(multiple=True, loops=True, combine_edges="sum")
+
+
+'''
+
+
+# Code to read ncol file as mutual network:
+
+'''
 g_mutual = Graph()
+g_mutual = g_mutual.Read_Ncol("graph.ncol", names=True, weights=True, directed=True)
 
+edges_2delete = []
 
-#g_nonmutual.Read_Ncol("graph.ncol", weights=True, directed=True)
-#g_nonmutual.to_undirected(mode="collapse")
+for e in g_mutual.es:
+    if g_mutual.get_eid(e.target, e.source, directed=True, error=False) == -1:
+        edges_2delete.append(e)
+
+g_mutual.delete_edges(edges_2delete)
+
+# Remove arrows from each edge.
+g_mutual.to_undirected(mode="each")
+
+# Simplifies a graph by removing self-loops and/or multiple edges. 
+g_mutual = g_mutual.simplify(multiple=True, loops=True, combine_edges="sum")
+
 '''
-g_mutual.Read_Ncol("graph.ncol", weights=True, directed=True)
-g_mutual.to_undirected(mode="mutual")
+
 '''
+https://igraph.org/python/doc/igraph.GraphBase-class.html
+
+'''
+

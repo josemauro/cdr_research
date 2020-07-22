0a49f8ebb5be57ec2c16fe2f6173b555808403a6
jose <z.mauro@live.com>
2020-07-22 10:48:17 -0300

    Add function to generate histogram degree distribution.

--- 2_basic_network_characteristics/network_characteristics.py
+++ 2_basic_network_characteristics/network_characteristics.py
@@ -1,8 +1,9 @@
 import os
-
+import collections
 from igraph import Graph
 from random import randint
 from igraph import *
+import matplotlib.pyplot as plt
 
 NETWORK_PATH = ''
 
@@ -25,11 +26,11 @@ def inspect_network():
         if v>=0.6 and v<0.7:
             color = "pink"
         if v>=0.7:
-            color = "red" 
+            color = "red"
         return color
 
     '''
-    Since this 
+    Since this
     '''
     #out_dir = './2_inspecting_sample_out'
     #num_files = len([name for name in os.listdir(out_dir) if os.path.isfile(name)])
@@ -69,6 +70,25 @@ def basic_metrics(g):
     print('Min degree: ', min_degree)
     print('Max weight:', max(g.es['weight']))
     print('Min weight:', min(g.es['weight']))
+
+def degree_histogram(g_network):
+    '''Plot a degree histogram using matplotlib.'''
+    # degree sequence
+    degree_sequence = sorted([d for n, d in g_network.degree()], reverse=True)
+    # print "Degree sequence", degree_sequence
+    degreeCount = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degreeCount.items())
+
+    fig, ax = plt.subplots()
+    plt.bar(deg, cnt, width=0.80, color='b')
+
+    plt.title("Degree Histogram")
+    plt.ylabel("Count")
+    plt.xlabel("Degree")
+    ax.set_xticks([d + 0.4 for d in deg])
+    ax.set_xticklabels(deg)
+
+    plt.show()
 
 def weight_distribution(weights, prefix):
     # x is a list that contains all degrees existing in the graph
@@ -126,56 +146,46 @@ def weight_distribution(weights, prefix):
     plt.savefig('ccdf_plot.png')
     plt.scatter(x, y, alpha=0.5)
 
-def degree_distribution(degrees, prefix, total_nodes):
+def degree_distribution(degrees, count, total_nodes):
     '''This function plot the degree distribuitions.'''
-    # x is a list that contains all degrees existing in the graph
-    max_x = max(degrees)
-    x_degree = list(range(0, max_x + 1)) # '+ 1' is used to include max_x
-
-    #y is a list that contains all frequency for each degree (in x)
-    y_freq = [degrees.count(i) for i in x_degree]
-
-    '''
     # Scatter Plot
-    plt.scatter(x_degree, y_freq, alpha=0.5)
+    plt.scatter(degrees, count, alpha=0.5)
     plt.title('Degree distribuition')
     plt.xlabel('k')
     plt.ylabel('P(k)')
-    plt.savefig(prefix+'degree_scatter_plot.png')
+    plt.savefig('degree_scatter_plot.png')
     plt.clf()
     print('Scatter plot done!')
-    '''
-    # Plot CCDF
-    y_ccdf = []
-    x_ccdf = x_degree
 
+    # Plot CCDF
     accumulated = 0
-    for i in y_freq:
-        accumulated = i/float(total_nodes) + accumulated
+    y_ccdf = []
+    for i in count:
+        accumulated = i/total_nodes + accumulated
         y_ccdf.append((1 - accumulated))
 
 
-    plt.scatter(x_ccdf, y_ccdf, alpha=0.5, marker='x')
+    plt.scatter(degrees, y_ccdf, alpha=0.5, marker='x')
     plt.title('CCDF')
     plt.xlabel('Grau k')
     plt.ylabel('P[D=k]')
     plt.yticks([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
     plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-    plt.savefig(f'{prefix}degree_ccdf_plot.png')
+    plt.savefig(f'degree_ccdf_plot.png')
     print(f'{prefix}CCDF plot done!')
 
 def count_num_calls(g, e_1):
     x_num = 0
     y_duration = 0
-    
+
     for e_2 in g.es:
         if (e_1.tuple == e_2.tuple):
             x_num = x_num + 1
             y_duration = y_duration + e_2['weight']
-    
+
     return x_num, y_duration
 
-def plot_scatter_dur_num(x, y)
+def plot_scatter_dur_num(x, y):
     # Plot
     plt.scatter(x, y, alpha=0.5)
     plt.title('Scatter plot of call duration and number of calls')
@@ -183,9 +193,9 @@ def plot_scatter_dur_num(x, y)
     plt.ylabel('duration calls')
     plt.savefig(prefix+'num_duration_scatter_plot.png')
     plt.clf()
-    
-g_mutual = Graph()
-g_mutual = g_mutual.Read_Ncol(NETWORK_PATH, weights=True, directed=True)
+
+#g_mutual = Graph()
+#g_mutual = g_mutual.Read_Ncol(NETWORK_PATH, weights=True, directed=True)
 
 
 

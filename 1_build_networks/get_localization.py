d0399fb1bb4be8921a325f7d1217f3721e2eb8ff
jose <z.mauro@live.com>
2020-10-21 15:35:14 -0300

    Simplify the code removing unused code

--- 1_build_networks/get_localization.py
+++ 1_build_networks/get_localization.py
@@ -1,61 +1,19 @@
 from igraph import Graph
-from unidecode import unidecode
-from geopy.geocoders import Nominatim
 
 import csv
 
 
-FILE_LOC_PATH = 'data/residencia_presumida'
-
-def generate_id_locations():
-    ''' Create an id for each location.'''
-    locations = []
-
-    with open(FILE_LOC_PATH, newline='') as csv_file:
-        spamreader = csv.reader(csv_file, delimiter='\t')
-        for row in spamreader:
-            row = ' '.join(row[1:])
-            locations.append(row)
-
-    locations = list(set(locations))
-    locations.sort()
-
-    #print(locations)
-
-    index = list(range(0,len(locations)))
-
-    #id_location_dict = dict(zip(index, locations))
-    location_id_dict = dict(zip(locations, index))
-
-
-    return location_id_dict
-
-
-def get_lat_long(location):
-    geolocator = Nominatim(user_agent="mestrado")
-    loc = geolocator.geocode(f'{location}, rio de janeiro')
-    return loc.latitude, loc.longitude
-
-def write_file_mapping_id_locations(location_id_dict):
-    '''Create a file named containing id and location in each line.'''
-    file_map_path = 'data/residencia_presumida_mapping.csv'
-
-    with open(file_map_path, 'w') as file_write:
-        for location, index in location_id_dict.items():
-            lat, long = get_lat_long(location)
-            out = f'{index}, {location}, {lat}, {long}\n'
-            file_write.write(out)
+FILE_LOC_PATH = 'data/user_residence-antenna.csv'
 
 def create_vertex_locations_dict():
-    ''' Create a dict containing vertex as key and location as value..'''
+    ''' Create a dict containing vertex as key and location as value.'''
     vertex_loc_list = []
 
     with open(FILE_LOC_PATH, newline='') as csv_file:
-        spamreader = csv.reader(csv_file, delimiter='\t')
+        spamreader = csv.reader(csv_file, delimiter=';')
         for row in spamreader:
             vertex_loc_list.append(row)
     vertex_loc_dict = dict(vertex_loc_list)
-
     return vertex_loc_dict
 
 def add_location_in_network(network, vertex_loc_dict):
@@ -74,27 +32,25 @@ def add_location_in_network(network, vertex_loc_dict):
         try:
             # The substring '21' is missing for some reason at indentifiers in
             # 'residencia_presumida' file.
-            location = vertex_loc_dict['21' + v['name']]
-            v['location'] = str(location_id_dict[ location ])
+            location = vertex_loc_dict[v['name']]
+            v['location'] = location
         except KeyError:
             vertices_to_delete.append(v.index)
             #v['location'] = 'None'
             cont = cont + 1
 
     g.delete_vertices(vertices_to_delete)
+    g = g.clusters().giant()
     print(v.index)
     g.write_gml(f'./out/{network_name}.gml')
     print(f'Finished. {str(cont)} nodes without location at {network_name}.')
 
 
-location_id_dict = generate_id_locations()
-
-write_file_mapping_id_locations(location_id_dict)
-
 vertex_loc_dict = create_vertex_locations_dict()
 
 #networks = ['largest_nonmutual.ncol', 'largest_mutual.ncol']
-networks = ['largest_rio_1d_nonmutual.ncol']
+networks = ['largest_32_RISJDR_mutual.ncol',
+            'largest_32_RISJDR_nonmutual.ncol']
 
 for network in networks:
     add_location_in_network(network, vertex_loc_dict)
